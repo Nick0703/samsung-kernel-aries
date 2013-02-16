0b86a4a61f6c4afb4146af7004e19fb3b649ef6b
Nick0703 <lokesh.c703@gmail.com>
2013-02-15 21:38:29 -0500

    Revert "Fix scaling issue"
    This reverts commit 68b2b8dbb50b48aa3e980fb9e282c4057aa7c25a.

--- arch/arm/mach-s5pv210/cpufreq.c
+++ arch/arm/mach-s5pv210/cpufreq.c
@@ -33,6 +33,7 @@ static struct cpufreq_freqs freqs;
 static DEFINE_MUTEX(set_freq_lock);
 
 /* APLL M,P,S values for 1.4-1.0GHz/800MHz */
+#define APLL_VAL_1400   ((1 << 31) | (175 << 16) | (3 << 8) | 1)
 #define APLL_VAL_1300   ((1 << 31) | (325 << 16) | (6 << 8) | 1)
 #define APLL_VAL_1200	((1 << 31) | (150 << 16) | (3 << 8) | 1)
 #define APLL_VAL_1100   ((1 << 31) | (141 << 16) | (3 << 8) | 1)
@@ -76,9 +77,10 @@ enum s5pv210_dmc_port {
 };
 
 static struct cpufreq_frequency_table s5pv210_freq_table[] = {
-	{OC0, 1300*1000},
-	{OC1, 1200*1000},
-	{OC2, 1100*1000},
+	{OC0, 1400*1000},
+	{OC1, 1300*1000},
+	{OC2, 1200*1000},
+	{OC3, 1100*1000},
 	{L0, 1000*1000},
 	{L1, 800*1000},
 	{L2, 400*1000},
@@ -97,7 +99,7 @@ struct s5pv210_dvs_conf {
 
 #ifdef CONFIG_DVFS_LIMIT
 static unsigned int g_dvfs_high_lock_token = 0;
-static unsigned int g_dvfs_high_lock_limit = 7;
+static unsigned int g_dvfs_high_lock_limit = 6;
 static unsigned int g_dvfslockval[DVFS_LOCK_TOKEN_NUM];
 //static DEFINE_MUTEX(dvfs_high_lock);
 #endif
@@ -112,14 +114,18 @@ const unsigned long int_volt_max = 1250000;
 
 static struct s5pv210_dvs_conf dvs_conf[] = {
 	[OC0] = {
-		.arm_volt   = 1325000,
+		.arm_volt   = 1350000,
 		.int_volt   = 1125000,
 	},
 	[OC1] = {
+		.arm_volt   = 1325000,
+		.int_volt   = 1125000,
+	},
+	[OC2] = {
 		.arm_volt   = 1275000,
 		.int_volt   = 1100000,
 	},
-	[OC1] = {
+	[OC3] = {
 		.arm_volt   = 1275000,
 		.int_volt   = 1100000,
 	},
@@ -145,7 +151,7 @@ static struct s5pv210_dvs_conf dvs_conf[] = {
 	},
 };
 
-static u32 clkdiv_val[8][11] = {
+static u32 clkdiv_val[9][11] = {
 	/*
 	 * Clock divider value for following
 	 * { APLL, A2M, HCLK_MSYS, PCLK_MSYS,
@@ -153,15 +159,18 @@ static u32 clkdiv_val[8][11] = {
 	 *   ONEDRAM, MFC, G3D }
 	 */
 
-	/* OC0 : [1300/200/200/100][166/83][133/66][200/200] */
+	/* OC0 : [1400/200/200/100][166/83][133/66][200/200] */
+	{0, 6, 6, 1, 3, 1, 4, 1, 3, 0, 0},
+
+	/* OC1 : [1300/200/200/100][166/83][133/66][200/200] */
 	{0, 5.5, 5.5, 1, 3, 1, 4, 1, 3, 0, 0},
 
-	/* OC1 : [1200/200/100][166/83][133/66][200/200] */
+	/* OC2 : [1200/200/100][166/83][133/66][200/200] */
 	{0, 5, 5, 1, 3, 1, 4, 1, 3, 0, 0},
 
-	/* OC2 : [1100/200/200/100][166/83][133/66][200/200] */
+	/* OC3 : [1100/200/200/100][166/83][133/66][200/200] */
 	{0, 5, 5, 1, 3, 1, 4, 1, 3, 0, 0},
-
+	
 	/* L0 : [1000/200/100][166/83][133/66][200/200] */
 	{0, 4, 4, 1, 3, 1, 4, 1, 3, 0, 0},
 
@@ -186,7 +195,7 @@ static bool pllbus_changing = false;
 static int oc_value = 100;
 
 static unsigned long sleep_freq;
-static unsigned long original_fclk[] = {1300000, 1200000, 1100000, 1000000, 800000, 800000, 800000, 800000};
+static unsigned long original_fclk[] = {1400000, 1300000, 1200000, 1100000, 1000000, 800000, 800000, 800000, 800000};
 
 static u32 apll_values[sizeof(original_fclk) / sizeof(unsigned long)];
 #endif
@@ -379,13 +388,15 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 	cpufreq_notify_transition(&freqs, CPUFREQ_PRECHANGE);
 
 	/* Check if there need to change PLL */
-	if ((index <= L0) || (freqs.old >= s5pv210_freq_table[L0].frequency))
+	if ((index == OC0) || (priv_index == OC0))   //1.4GHz
 		pll_changing = 1;
-	else if ((index == L1) || (freqs.old == s5pv210_freq_table[L1].frequency))
+	else if ((index == L0) || (priv_index == L0))   // 1GHz
 		pll_changing = 1;
-	
+	else if ((index == L1) || (priv_index == L1))   // 800MHz
+		pll_changing = 1;
+
 	/* Check if there need to change System bus clock */
-	if ((index == L4) || (freqs.old == s5pv210_freq_table[L4].frequency))
+	if ((index == L4) || (priv_index == L4))
 		bus_speed_changing = 1;
 
 #ifdef CONFIG_LIVE_OC
@@ -513,12 +524,15 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 #else
 		switch (index) {
 		case OC0:
-			__raw_writel(APLL_VAL_1300, S5P_APLL_CON);
+			__raw_writel(APLL_VAL_1400, S5P_APLL_CON);
 			break;
 		case OC1:
-			__raw_writel(APLL_VAL_1200, S5P_APLL_CON);
+			__raw_writel(APLL_VAL_1300, S5P_APLL_CON);
 			break;
 		case OC2:
+			__raw_writel(APLL_VAL_1200, S5P_APLL_CON);
+			break;
+		case OC3:
 			__raw_writel(APLL_VAL_1100, S5P_APLL_CON);
 			break;
 		case L0:
@@ -804,7 +818,7 @@ void liveoc_update(unsigned int oc_value)
     while (s5pv210_freq_table[i].frequency != CPUFREQ_TABLE_END) {
 
 	index = s5pv210_freq_table[i].index;
-
+	
 	if (s5pv210_freq_table[i].frequency == policy->user_policy.min)
 	    index_min = index;
 
@@ -839,17 +853,17 @@ void liveoc_update(unsigned int oc_value)
 		else if (index == 1) 
 			dvs_conf[1].arm_volt = 1275000;	
 	}
-
+	
 	if (arm_voltages) {
 		arm_voltages[0] = dvs_conf[0].arm_volt; 
 		arm_voltages[1] = dvs_conf[1].arm_volt; 	  
 	}
-
+	  
 
 	fclk /= 1000;
 
 	divider = find_divider(fclk);
-
+	
 	apll_values[index] = ((1 << 31) | (((fclk * divider) / 24) << 16) | (divider << 8) | (1));
 
 	i++;
